95d89d049231c2392622a53541a2ffbe2a22aebf
oumarsow2003 <97905166+oumarsow2003@users.noreply.github.com>
2023-01-19 23:36:23 +0100

    Update FriendList.js

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -15,7 +15,7 @@ const FriendList = () => {
 }
   return (
     <div className='bg-white rounded w-96 flex flex-col  shadow-sm align-items-center justify-center'>
-        <img className='w-48 object-cover rounded-full h-48 mb-3' src={currentUser?.photoURL} alt="" />
+        <
         <h2 className='text-center text-3xl mb-3'>{currentUser?.displayName}</h2>
         <p className='text-center text-blue-700  cursor-pointer' onClick={logout}>Déconnexion</p>
         
